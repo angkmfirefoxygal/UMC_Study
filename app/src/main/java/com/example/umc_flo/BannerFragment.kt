bec5ee3eb5a4d965d430c5f9409511a5c2f38ef0
moonwon <angkmfirefoxgal@gmail.com>
2024-05-18 00:20:00 +0900

    20240518

---
+++ app/src/main/java/com/example/umc_flo/BannerFragment.kt
@@ -0,0 +1,27 @@
+package com.example.umc_flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.umc_flo.databinding.FragmentBannerBinding
+
+class BannerFragment (val imgRes : Int) : Fragment(){
+
+    lateinit var binding: FragmentBannerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding  = FragmentBannerBinding.inflate(inflater, container, false)
+
+
+        binding.bannerImageIv.setImageResource(imgRes)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/umc_flo/Song.kt
@@ -0,0 +1,6 @@
+package com.example.umc_flo
+
+data class Song(
+    val title : String = "",
+    val singer : String =""
+)
